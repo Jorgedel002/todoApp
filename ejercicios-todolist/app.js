ae769c42118ad8696dc36d305296684325b03ab6
Jorgedel002 <jorgedel002@gmail.com>
2023-02-10 04:25:33 -0300

    Primer commit

---
+++ ejercicios-todolist/app.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", function(){
+    let btnAdd = document.getElementById("btnAdd");
+    let btnAddTask = document.getElementById("addTask");
+    let inputTask = document.getElementById("taskInput");
+    let modal = document.getElementById("modal");
+    let taskContainer = document.getElementById("taskContainer");
+    let noTasksContainer = document.getElementById("noTasks");
+    let close = document.getElementById("close");
+    let tasks = []
+
+    btnAdd.addEventListener("click", function(){
+        modal.classList.add('open');
+        console.log("click")
+    })
+
+    close.addEventListener("click", function(){
+        modal.classList.remove('open');
+    })
+
+    btnAddTask.addEventListener("click", function(){
+        tasks.push({nombreTarea: inputTask.value})
+
+        taskContainer.innerHTML = ""
+        tasks.map(function(task, i){
+            let taskElement = document.createElement("div");
+            taskElement.classList.add("task");
+            taskElement.innerHTML = "<div>#"+i+"</div>" +" "+ task.nombreTarea;
+            
+            console.log(i)
+            
+            if(tasks.length > 0){
+                taskContainer.classList.remove("task-flex")
+                taskContainer.append(taskElement)
+            }else{
+                noTasksContainer.classList.add("d-none")
+            }
+            
+        })
+
+        inputTask.value = ""
+        modal.classList.remove('open');
+    })
+
+})
